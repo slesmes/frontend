289f1e16aa8a7b9e01f52d67028b32b5f7d67b6c
slesmes <santiagolesmesmarin@hotmail.com>
2024-08-26 00:40:26 -0500

    añadiendo el aside al proyecto cine

--- src/components/principal.tsx
+++ src/components/principal.tsx
@@ -1,10 +1,31 @@
 import '../styles/principal.css'
-export default function Principal(){
-    return(
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+export default function Principal() {
+
+    return (
         <main className="main">
-            <div className="aside">
-                this is the aside
-            </div>
+            <aside className="aside">
+                <div className="schedule-section">
+                    <label>Horario</label>
+                    <div className="time-selection">
+                        <label>De</label>
+                        <select>
+                            <option value="11:00 PM">11:00 PM</option>
+                            <option value="10:00 PM">10:00 PM</option>
+                        </select>
+                        <label> A las</label>
+                        <select>
+                            <option value="11:59 PM">11:59 PM</option>
+                            <option value="10:59 PM">10:59 PM</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="date-section">
+                    <label>Día</label>
+                    <Calendar className="custom-calendar" />
+                </div>
+            </aside>
             <div className="principal">
                 this is the principal content
             </div>
